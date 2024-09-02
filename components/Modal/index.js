7fc9b16ac59db522480520b636659ef0d4233d44
niharikak <niharika.kallem@empover.com>
2024-09-02 14:34:31 +0530

    customModalChnages

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal';
-import Image from 'next/image';
-import languagePopupImg from '@Images/languagePopup.svg';
-import Btn from '@components/Btn';
+import Image from 'next/image'; 
+import CloseIcon from '@Images/CloseIcon.svg';
 
 
-export default function ModalComponent({ show }) {
+export default function ModalComponent({ showModal, handleClose, content }) {
 
 
     const customStyles = {
@@ -17,59 +16,19 @@ export default function ModalComponent({ show }) {
             marginRight: '-50%',
             transform: 'translate(-50%, -50%)',
         },
-    };
+    }; 
 
-    const [showModal, setShowModal] = useState(true);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-        <div className='bg-[pink]'>
+     return (
+        <div>
             <Modal
                 isOpen={showModal}
                 style={customStyles}
-                contentLabel="Example Modal">
-                <button className='absolute right-0'>
-                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 18L18 6M6 6l12 12" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                contentLabel="languageSelectionModal">
+                <button className='absolute right-2 top-2 z-50' onClick={handleClose}>
+                    <Image src={CloseIcon} alt='closeIcon' width={19} height={19} /> 
                 </button>
-                <div>
-                    <div className='flex'>
-                        <div className='p-4'>
-                            <p className='font-bold xl:text-xl lg:text-lg md:text-base'>Select your preferred Language</p>
-
-                            <span className='my-4 block'>Select Language</span>
-
-                            <div className='flex gap-4 my-4'>
-                                <div className='border-[1px] border-secondaryColor px-2 py-1 rounded'>
-                                    <input type="radio" name="language" value="hindi" />
-                                    <label className="ml-2">Englidh</label>
-                                </div>
-
-                                <div className='border-[1px] border-secondaryColor px-2 py-1 rounded'>
-                                    <input type="radio" name="language" value="hindi" />
-                                    <label className="ml-2">Hindi</label>
-                                </div>
-
-                                <div className='border-[1px] border-secondaryColor px-2 py-1 rounded'>
-                                    <input type="radio" name="language" value="hindi" />
-                                    <label className="ml-2">Marathi</label>
-                                </div>
-
-                            </div>
-
-
-                            <div>
-
-                                <Btn text={'Submit'} bgColor={true} />
-
-
-                            </div>
-
-                        </div>
-                        <div className=''>
-                            <Image src={languagePopupImg} width={200} height={200} /></div>
-                    </div>
+                <div className='w-full'>
+                   {content}
                 </div>
             </Modal>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,98 @@
+import React, { useState } from 'react'
 import Layout from "@components/Layout";
 import { getLocaleProps } from "@utils";
 import HomePage from './home';
+import Image from 'next/image';
+import languagePopupImg from '@Images/languagePopup.svg';
+import Btn from '@components/Btn';
 import Modal from "@components/Modal";
 
 export default function Home({ locale }) {
+  const [showModal, setShowModal] = useState(true);
+  const handleClose = () => setShowModal(false);
+
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
   return (
-   
+
     <>
       <Layout currentPage={"home"}>
         <HomePage locale={locale} />
       </Layout>
+      <Modal showModal={showModal} handleClose={handleClose} content={
+        <div className='flex items-center sm:flex-row flex-col-reverse w-full'>
+          <div className='px-4 py-10'>
+            <p className='font-bold  text-xl'>Select your preferred<br></br> Language</p>
+            <span className='mt-6 block text-sm'>Select Language</span>
+            <div className='flex gap-4 my-4'>
+              <div className={`radio-group border-[1px] border-black px-2 py-1 rounded ${selectedLanguage === 'english' ? 'border-secondaryColor' : ''}`}>
+                <input
+                  type="radio"
+                  name="language"
+                  value="english"
+                  id="english"
+                  className=""
+                  onChange={handleLanguageChange}
+                />
+                <label htmlFor="english" className={`ml-2 ${selectedLanguage === 'english' ? 'text-secondaryColor' : ''} font-medium`}>
+                  English
+                </label>
+              </div>
 
-      <Modal show={true} />
-      </>
+              <div className={`radio-group border-[1px] border-black px-2 py-1 rounded ${selectedLanguage === 'hindi' ? 'border-secondaryColor' : ''}`}>
+                <input
+                  type="radio"
+                  name="language"
+                  value="hindi"
+                  id="hindi"
+                  className=""
+                  onChange={handleLanguageChange}
+                />
+                <label htmlFor="hindi" className={`ml-2 ${selectedLanguage === 'hindi' ? 'text-secondaryColor' : ''} font-medium`}>
+                  Hindi
+                </label>
+              </div>
 
-      );
+              <div className={`radio-group border-[1px] border-black px-2 py-1 rounded ${selectedLanguage === 'marathi' ? 'border-secondaryColor' : ''}`}>
+                <input
+                  type="radio"
+                  name="language"
+                  value="marathi"
+                  id="marathi"
+                  className=""
+                  onChange={handleLanguageChange}
+                />
+                <label htmlFor="marathi" className={`ml-2 ${selectedLanguage === 'marathi' ? 'text-secondaryColor' : ''} font-medium`}>
+                  Marathi
+                </label>
+              </div>
+            </div>
+
+            <div>
+              <Btn text={'Submit'} bgColor={true} />
+            </div>
+          </div>
+          <div className="sm:relative sm:w-[300px] sm:h-[300px] w-full h-auto">
+            <Image
+              src={languagePopupImg}
+              layout='responsive'
+              width={300}
+              height={300}
+              className='languagePopupImg'
+              alt='languagePopupImg'
+            />
+          </div>
+
+        </div>
+      } />
+    </>
+
+  );
 }
-      export async function getServerSideProps(context) { 
+export async function getServerSideProps(context) {
   return await getLocaleProps(context);
 }
