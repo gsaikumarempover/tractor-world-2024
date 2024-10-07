bdd989bb9c9af68798020182dda4bd51ceba45bc
niharikakallem <niharikak1919@gmail.com>
2024-10-07 11:10:05 +0530

    build issue

--- components/EMI/RightSection/chartFooter/index.js
+++ components/EMI/RightSection/chartFooter/index.js
@@ -5,10 +5,10 @@ const ChartFooter = ({ emi, principal, interestPayable }) => {
     <div className="w-2/3 block m-auto">
     <div className="flex flex-col space-y-2">
     <div className="flex justify-between items-center">
-      <div className="flex items-center"><span class="w-[10px] h-[10px] border-[2px] bg-orange-400 border-orange-400 mx-4 align-middle"></span>
+      <div className="flex items-center"><span className="w-[10px] h-[10px] border-[2px] bg-orange-400 border-orange-400 mx-4 align-middle"></span>
       <label className="text-gray-700 font-semibold">Principal</label>
       </div>
-      <div clasclassNames="flex-grow text-right mb-4">
+      <div className="flex-grow text-right mb-4">
        &#8377;{principal.toLocaleString('en-IN')}
         </div>
         </div>
