ac541751c628d3cf1c4eddc112fb0d463c8da4b3
gurram-saikumar <saikumar.g@empover.com>
2025-03-12 13:02:49 +0530

    Update webpack configuration to prevent 'fs' resolution on client side and improve alias definitions

--- next.config.js
+++ next.config.js
@@ -1,25 +1,30 @@
 const path = require('path');
 const { i18n } = require('./next-i18next.config');
 
- module.exports = {
+module.exports = {
   reactStrictMode: true,
   i18n,
-  webpack: (config) => {
-    config.resolve.alias['@locales'] = path.join(__dirname, 'public','locales');
+  webpack: (config, { isServer }) => {
+    // Prevent Webpack from resolving 'fs' on the client side
+    if (!isServer) {
+      config.resolve.fallback = { fs: false };
+    }
+
+    config.resolve.alias['@locales'] = path.join(__dirname, 'public', 'locales');
     config.resolve.alias['@Images'] = path.join(__dirname, 'public', 'images');
     config.resolve.alias['@components'] = path.join(__dirname, 'components');
     config.resolve.alias['@service'] = path.join(__dirname, 'lib');
     config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
     config.resolve.alias['@helpers'] = path.join(__dirname, 'helpers');
-    config.resolve.alias['@store']=path.join(__dirname,'store','slices');
-    
+    config.resolve.alias['@store'] = path.join(__dirname, 'store', 'slices');
+
     return config;
   },
   env: {
     WORDPRESS_API_URL: process.env.WORDPRESS_API_URL,
-    LOCATION_GET_API: process.env.LOCATION_GET_API
+    LOCATION_GET_API: process.env.LOCATION_GET_API,
   },
   images: {
-    domains: ['tractorworld.empover.com']
-  }
+    domains: ['tractorworld.empover.com'],
+  },
 };
